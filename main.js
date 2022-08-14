dd1e70947bfb32de4f17c8ab1f87b812481ea980
sumitpathak914 <108678691+sumitpathak914@users.noreply.github.com>
2022-08-14 16:59:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+function event1(){
+    alert("web site in progress")
+}
+function event2(){
+    alert("web site in progress")
+}
+function event3(){
+    alert("web site in progress")
+}
+function event4(){
+    alert("web site in progress")
+}
+function event5(){
+    alert("web site in progress")
+}
+function event6(){
+    alert("web site in progress")
+}
+function event7(){
+    alert("web site in progress")
+}
+function event8(){
+    alert("web site in progress")
+}
+function event9(){
+    alert("web site in progress")
+}
+function event10(){
+    alert("web site in progress")
+}
+function event11(){
+    alert("web site in progress")
+}
+function event12(){
+    alert("web site in progress")
+}
+function event13(){
+    alert("web site in progress")
+}
+function event14(){
+    alert("web site in progress")
+}
+function event15(){
+    alert("web site in progress")
+}
+function event16(){
+    alert("web site in progress")
+}
+function event17(){
+    alert("web site in progress")
+}
+function event18(){
+    alert("web site in progress")
+}
+function event19(){
+    alert("web site in progress")
+}
+function event20(){
+    alert("web site in progress")
+}
+function event21(){
+    alert("web site in progress")
+}
+function event22(){
+    alert("web site in progress")
+}
+function event23(){
+    alert("web site in progress")
+}
+function event24(){
+    alert("web site in progress")
+}
+function event25(){
+    alert("web site in progress")
+}
+function event26(){
+    alert("web site in progress")
+}
+function event27(){
+    alert("web site in progress")
+}
+function event28(){
+    alert("web site in progress")
+}
+function event29(){
+    alert("web site in progress")
+}
+function event30(){
+    alert("web site in progress")
+}
